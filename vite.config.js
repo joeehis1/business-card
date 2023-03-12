7aca7e0bcae3fe45ca5b3d30afd7935675efd8d7
Joseph Ehigiator <joeehis1@gmail.com>
2023-03-12 12:59:33 -0400

    updated config files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/joeehis1/business-card",
   plugins: [react()],
 })
